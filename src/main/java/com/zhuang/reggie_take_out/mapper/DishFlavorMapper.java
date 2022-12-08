dc558f1c268ad31cb5ae0484ee42f378bb6f6eec
Kang-xiao-zhuang <2247830091@qq.com>
2022-12-08 14:41:14 +0800

    commit mapper code

---
+++ src/main/java/com/zhuang/reggie_take_out/mapper/DishFlavorMapper.java
@@ -0,0 +1,15 @@
+package com.zhuang.reggie_take_out.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.zhuang.reggie_take_out.entity.DishFlavor;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * description: DishFlavorMapper
+ * date: 2022/12/7 22:42
+ * author: Zhuang
+ * version: 1.0
+ */
+@Mapper
+public interface DishFlavorMapper extends BaseMapper<DishFlavor> {
+}
