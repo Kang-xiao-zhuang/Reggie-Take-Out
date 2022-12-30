813518e49a0f2cefc1bf9b81ae5356c0d1375570
Kang-xiao-zhuang <2247830091@qq.com>
2022-12-30 22:29:39 +0800

    订单明细

---
+++ src/main/java/com/zhuang/reggie_take_out/mapper/OrderDetailMapper.java
@@ -0,0 +1,10 @@
+package com.zhuang.reggie_take_out.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.zhuang.reggie_take_out.entity.OrderDetail;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface OrderDetailMapper extends BaseMapper<OrderDetail> {
+
+}
---
+++ src/main/java/com/zhuang/reggie_take_out/service/OrderDetailService.java
@@ -0,0 +1,8 @@
+package com.zhuang.reggie_take_out.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.zhuang.reggie_take_out.entity.OrderDetail;
+
+public interface OrderDetailService extends IService<OrderDetail> {
+
+}
