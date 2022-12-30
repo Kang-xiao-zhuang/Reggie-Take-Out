2fb095a6799e1bc68ed5b1f70773bc391f14d3eb
Kang-xiao-zhuang <2247830091@qq.com>
2022-12-30 22:30:46 +0800

    用户短信功能

---
+++ src/main/java/com/zhuang/reggie_take_out/service/UserService.java
@@ -0,0 +1,7 @@
+package com.zhuang.reggie_take_out.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.zhuang.reggie_take_out.entity.User;
+
+public interface UserService extends IService<User> {
+}
