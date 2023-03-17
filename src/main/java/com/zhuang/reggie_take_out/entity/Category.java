04bedf90d026f89a286b6041c43b7fc25943d225
Kang-xiao-zhuang <2247830091@qq.com>
2023-03-17 15:00:16 +0800

    修改代码

--- src/main/java/com/zhuang/reggie_take_out/entity/Category.java
+++ src/main/java/com/zhuang/reggie_take_out/entity/Category.java
@@ -1,9 +1,7 @@
 package com.zhuang.reggie_take_out.entity;
 
 import com.baomidou.mybatisplus.annotation.FieldFill;
-import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
-import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.*;
 
 import java.io.Serializable;
