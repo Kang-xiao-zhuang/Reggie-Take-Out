17918c6b825f0f089cbe58feab63fe590a2d85f9
Kang-xiao-zhuang <2247830091@qq.com>
2023-03-21 11:37:08 +0800

    优化代码

--- src/main/java/com/zhuang/reggie_take_out/common/R.java
+++ src/main/java/com/zhuang/reggie_take_out/common/R.java
@@ -19,17 +19,17 @@ public class R<T> implements Serializable {
     private T data;
 
     //动态数据
-    private Map<String,Object> map = new HashMap<>();
+    private Map<String, Object> map = new HashMap<>();
 
     public static <T> R<T> success(T object) {
-        R<T> r = new R<T>();
+        R<T> r = new R<>();
         r.data = object;
         r.code = 1;
         return r;
     }
 
     public static <T> R<T> error(String msg) {
-        R r = new R();
+        R<T> r = new R<>();
         r.msg = msg;
         r.code = 0;
         return r;
